6717a5e8d36f100284c9285cbf4983ad93097708
Alexander Teye <bizzle.teye@gmail.com>
2025-09-29 08:30:42 -0700

    Delete all files from repository

--- basic_operations.py
+++
@@ -1,9 +0,0 @@
-number1 = 10
-number2 = 5
-sum_result = number1 + number2
-difference_result = number1 - number2
-product_result = number1 * number2 
-
-print ("Addition of ", number1, "and", number2, "is", sum_result)
-print ("difference of", number1, "and", number2, "is", difference_result)
-print ("product of", number1, "and", number2, "is", product_result)
