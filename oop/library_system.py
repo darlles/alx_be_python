760bb3b8134ead6de3615d2f63faa3a517d6a9b3
Alexander <bizzle.teye@gmail.com>
2025-10-17 01:38:52 -0700

    commit system

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,27 +1,27 @@
 class Book:
-    def __init__(self, title:str, author:str):
+    def __init__(self, title: str, author: str):
         self.title = title
         self.author = author
 
-    def get_details(self):
+    def __str__(self):
         return f"Book: {self.title} by {self.author}"
 
 
 class EBook(Book):
-    def __init__(self, title:str, author:str, file_size:int):
+    def __init__(self, title: str, author: str, file_size: int):
         super().__init__(title, author)
         self.file_size = file_size
 
-    def get_details(self):
+    def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
 class PrintBook(Book):
-    def __init__(self, title:str, author:str, page_count:int):
+    def __init__(self, title: str, author: str, page_count: int):
         super().__init__(title, author)
         self.page_count = page_count
 
-    def get_details(self):
+    def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
@@ -34,4 +34,4 @@ class Library:
 
     def list_books(self):
         for book in self.books:
-            print(book.get_details())
+            print(book)  # __str__ is automatically called
